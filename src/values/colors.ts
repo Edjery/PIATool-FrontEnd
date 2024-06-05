abfcdef64ec8a97c8e6f74a2a303419e36e5ce1d
Edjery <edisgumbao@gmail.com>
2024-06-05 12:49:07 +0800

    fix: main content bg color

--- src/values/colors.ts
+++ src/values/colors.ts
@@ -1,9 +1,8 @@
-export const mainContentPrimaryColor = "F4F5F9"
-export const sideBarPrimaryColor = "#353942"
-export const sideBarPrimaryTextColor = "#F4F5F9"
+export const mainContentPrimaryColor = "#F4F5F9";
+export const sideBarPrimaryColor = "#353942";
+export const sideBarPrimaryTextColor = "#F4F5F9";
 
 export const usepPrimaryColor = "#760000";
 export const usepSecondaryColor = "#FFFF00";
 export const usepTextPrimaryColor = "white";
 export const usepTextSecondaryColor = "#FFFF00";
-
