bb9994998c7f69d1254777a137b3a4f1a065f3fa
Edjery <edisgumbao@gmail.com>
2024-07-03 17:15:01 +0800

    Fix: router error

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,6 +1,4 @@
 import { createBrowserRouter } from "react-router-dom";
-import sampleAssessmentInputs from "../components/assessmentProcess/initialValues/sampleAssessmentInputs";
-import AssessmentDocument from "../components/document/AssessmentDocument";
 import Assessment from "../components/mainContent/Assessment";
 import UserHomePage from "../components/mainContent/UserHomePage";
 import ErrorPage from "../components/pages/ErrorPage";
@@ -19,12 +17,6 @@ const router = createBrowserRouter([
       {
         path: "/assessment",
         element: <Assessment />,
-      },
-      {
-        path: "/assessment/view",
-        element: (
-          <AssessmentDocument AssessmentInputs={sampleAssessmentInputs} />
-        ),
       },
     ],
   },
